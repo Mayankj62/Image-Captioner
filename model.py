00468f3795273b0c6ec65869d5ffef94702453e6
Mayank Jain <mayankj2112@gmail.com>
2020-04-25 21:07:32 -0400

    Update model.py

--- model.py
+++ model.py
@@ -21,6 +21,8 @@ class EncoderCNN(nn.Module):
         features = self.embed(features)
         return features
     
+        
+    
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
@@ -38,14 +40,12 @@ class DecoderRNN(nn.Module):
         self.fc = nn.Linear(hidden_size, vocab_size)
                             
     def forward(self, features, captions):
-        captions = captions[:, :captions.size()[1]]
-        # For some reason I get an error that the line below needs to be a long type, when I change it to long, it says it needs a float type, then I change it to a float type and it stops complaining. Weird ...
-        captions = captions.type(torch.float)
-
+        captions = captions[:, :captions.size()[1]-1]
+        features = features.unsqueeze(1)
         embeded = self.word_embedding(captions)
         
         
-        inp = torch.cat((embeded, features))
+        inp = torch.cat((embeded, features), 1)
         
         x, _ = self.lstm(inp)
         x = self.fc(x)
@@ -54,8 +54,23 @@ class DecoderRNN(nn.Module):
 
     def sample(self, inputs, states=None, max_len=20):
         " accepts pre-processed image tensor (inputs) and returns predicted sentence (list of tensor ids of length max_len) "
-#         sentence = []
+        sentence = [0]
+        hidden = 0
         
-#         while len(sentence) < (max_len - 1):
-#             encoded = self
-        pass
+        while len(sentence) != (max_len+1):
+            
+            output, states = self.lstm(inputs, states)
+            output = self.fc(output.squeeze(dim = 1))
+
+            _, predicted_index = torch.max(output, 1)
+            print(predicted_index)
+            sentence.append(int(predicted_index))
+            inputs = self.word_embedding(predicted_index)
+            
+            if predicted_index == 1:
+                break
+           
+        if sentence[-1] != 1:
+            sentence.append(1)
+            
+        return sentence
